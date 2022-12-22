e9b638a4cff938f201f19db341a494d2e1537427
OlegRozhok <oleg.yur.rozhok@gmail.com>
2022-12-22 16:51:40 +0300

    registration

--- app/views.py
+++ app/views.py
@@ -81,7 +81,7 @@ class ProductDetail(View):
         return render(request, "app/product_detail.html", locals())
 
 
-@method_decorator(login_required, name='dispatch')
+
 class CustomerRegistrationView(View):
     def get(self, request):
         form = CustomerRegistrationForm()
